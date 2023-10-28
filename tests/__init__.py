278cb21a44ce74960d1d6373d078bd7af97f8d59
Bernat Brunet <bernat@nan-tic.com>
2023-10-28 12:38:58 +0200

    Fix test import.

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,6 +1,3 @@
 # This file is part account_statement_enable_banking module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
-from .test_account_statement_enable_banking import suite
-
-__all__ = ['suite']
