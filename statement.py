289bcf47974268f6695b6698196c31525d9e8203
Bernat Brunet <bernat@nan-tic.com>
2023-11-03 10:37:22 +0100

    When search a suggested line ensure that is related to the origin where the search is required.

--- statement.py
+++ statement.py
@@ -1144,6 +1144,7 @@ class Origin(Workflow, metaclass=PoolMeta):
             SuggestedLine.create(suggesteds)
             if suggested_use:
                 suggest_lines = SuggestedLine.search([
+                    ('origin', '=', self)
                     ('name', '=', suggested_use)
                     ], limit=1)
                 if suggest_lines:
