fa086af3c4d515b990199a2a24e1f40320b9fd40
Bernat Brunet <bernat@nan-tic.com>
2024-01-21 09:41:13 +0100

    Fix when search possible reconciliation when import model 43.

--- statement_aeb43.py
+++ statement_aeb43.py
@@ -7,6 +7,20 @@ from trytond.pool import Pool, PoolMeta
 class ImportStatement(metaclass=PoolMeta):
     __name__ = 'account.statement.import'
 
+    def do_import_(self, action):
+        pool = Pool()
+        Statement = pool.get('account.statement')
+        StatementOrigin = pool.get('account.statement.origin')
+
+        action, data = super().do_import_(action)
+        statement_ids = data.get('res_id', None)
+        if statement_ids:
+            statements = Statement.browse(statement_ids)
+            origins = [o for s in statements for o in s.origins]
+            # Get the suggested lines for each origin created
+            StatementOrigin._search_reconciliation(origins)
+        return action, data
+
     def aeb43_statement(self, account):
         statement = super().aeb43_statement(account)
         statement.start_date = datetime.combine(account.initialDate,
@@ -16,17 +30,10 @@ class ImportStatement(metaclass=PoolMeta):
         return statement
 
     def aeb43_origin(self, statement, transaction):
-        pool = Pool()
-        StatementOrigin = pool.get('account.statement.origin')
-
         origin, = super().aeb43_origin(statement, transaction)
         origin.state = 'registered'
-        if origin.number is None and statement and statement.journal:
+        if statement and statement.journal:
             journal = statement.journal
             origin.number = journal.account_statement_origin_sequence.get()
 
-        origins = [origin]
-        # Get the suggested lines for each origin created
-        StatementOrigin._search_reconciliation(origins)
-
-        return origins
+        return [origin]
