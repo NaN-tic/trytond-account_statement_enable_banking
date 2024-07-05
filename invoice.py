2ee37c36fb04d8c0484ca6697aedff9a1938562f
Bernat Brunet <bernat@nan-tic.com>
2024-07-05 08:03:31 +0200

    Fix. As it's allowed to have account moves with more than one party on it need to update the lines_to_pay and reconciliation_lines functional fields are 'getted'.

--- invoice.py
+++ invoice.py
@@ -15,41 +15,32 @@ class Invoice(metaclass=PoolMeta):
     @classmethod
     def get_lines_to_pay(cls, invoices, name):
         pool = Pool()
-        MoveLine = pool.get('account.move.line')
-        AdditionalMove = pool.get('account.invoice-additional-account.move')
-        line = MoveLine.__table__()
-        invoice = cls.__table__()
-        additional_move = AdditionalMove.__table__()
-        cursor = Transaction().connection.cursor()
+        Line = pool.get('account.move.line')
 
-        lines = defaultdict(list)
-        for sub_ids in grouped_slice(invoices):
-            red_sql = reduce_ids(invoice.id, sub_ids)
-            query = (invoice
-                .join(line,
-                    condition=((invoice.move == line.move)
-                        & (invoice.account == line.account)
-                        & (invoice.party == line.party)))
-                .select(
-                    invoice.id.as_('invoice'),
-                    line.id.as_('line'),
-                    line.maturity_date.as_('maturity_date'),
-                    where=red_sql))
-            query |= (invoice
-                .join(additional_move,
-                    condition=additional_move.invoice == invoice.id)
-                .join(line,
-                    condition=((additional_move.move == line.move)
-                        & (invoice.account == line.account)
-                        & (invoice.party == line.party)))
-                .select(
-                    invoice.id.as_('invoice'),
-                    line.id.as_('line'),
-                    line.maturity_date.as_('maturity_date'),
-                    where=red_sql))
-            cursor.execute(*query.select(
-                    query.invoice, query.line,
-                    order_by=query.maturity_date.nulls_last))
-            for invoice_id, line_id in cursor:
-                lines[invoice_id].append(line_id)
+        lines = super().get_lines_to_pay(invoices, name)
+        for invoice_id, lines_id in lines.items():
+            invoice = cls(invoice_id)
+            new_lines = []
+            for line in Line.search([('id', 'in', lines_id)]):
+                if line.move.origin == invoice and line.party == invoice.party:
+                    new_lines.append(line.id)
+            lines[invoice_id] = new_lines
         return lines
+
+    # Need to refactorize the fleid function, to allow to have all
+    # the statement lines from ana origin in the same account move.
+    def get_reconciliation_lines(self, name):
+        pool = Pool()
+        Line = pool.get('account.move.line')
+
+        lines = super().get_reconciliation_lines(name)
+        if not lines:
+            return lines
+        new_lines = set()
+        reconciliation_lines_to_pay = [x.reconciliation
+            for x in self.lines_to_pay if x.reconciliation]
+        for line in Line.search([('id', 'in', lines)]):
+            if (line.party == self.party
+                    and line.reconciliation in reconciliation_lines_to_pay):
+                new_lines.add(line)
+        return [x.id for x in sorted(new_lines, key=lambda x: x.date)]
