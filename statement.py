67adeb5b6ad621b691bddc04edc63178ce7474e4
Àngel Àlvarez <angel@nan-tic.com>
2024-09-23 14:11:28 +0200

    add move_state equal to posted when search for conciliation

--- statement.py
+++ statement.py
@@ -175,6 +175,7 @@ class Line(metaclass=PoolMeta):
             If(Bool(Eval('account')),
                 ('account', '=', Eval('account')),
                 ()),
+            ('move_state', '=', 'posted'),
             ('account.reconcile', '=', True),
             ('state', '=', 'valid'),
             ('reconciliation', '=', None),
@@ -2085,6 +2086,7 @@ class AddMultipleMoveLinesStart(ModelView):
                ),
             ('account.reconcile', '=', True),
             ('state', '=', 'valid'),
+            ('move_state', '=', 'posted'),
             ('reconciliation', '=', None),
             ], required=True)
 
