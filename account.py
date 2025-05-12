762c8be258b3bec572a66b1ac60bb4b6d5a16e7c
Raimon Esteve <raimonesteve@gmail.com>
2025-05-12 14:09:04 +0200

    Use ModelStorage hooks instead of extending create/write/delete From changeset 5aeff66acd215e3f2d874a7112712e917c6ffe7e

--- account.py
+++ account.py
@@ -57,15 +57,6 @@ class MoveLine(metaclass=PoolMeta):
         return super()._get_origin() + ['account.statement.origin']
 
     @classmethod
-    def check_modify(cls, *args, **kwargs):
-        # It's needed to modify the lines even if the move is in 'posted'
-        # state.
-        if Transaction().context.get('from_account_statement_origin',
-                False):
-            return
-        return super().check_modify(*args, **kwargs)
-
-    @classmethod
     def reconcile(cls, *lines_list, date=None, writeoff=None, description=None,
             delegate_to=None):
         pool = Pool()
