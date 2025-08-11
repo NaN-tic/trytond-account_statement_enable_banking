3b4a16585c5462159e39b115302bd7644f6c9dba
Albert Cervera i Areny <albert@nan-tic.com>
2025-08-11 12:27:51 +0200

    Incorporate latest changes.

--- tests/test_module.py
+++ tests/test_module.py
@@ -7,7 +7,6 @@ from trytond.tests.test_tryton import ModuleTestCase
 class AccountStatementEnableBankingTestCase(ModuleTestCase):
     'Test Account Statement Enable Banking module'
     module = 'account_statement_enable_banking'
-    extras = ['account_statement_aeb43', 'analytic_account', 'account_bank',
-    'account_es']
+    extras = ['account_statement_aeb43', 'analytic_account']
 
 del ModuleTestCase
